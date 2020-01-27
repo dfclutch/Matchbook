3a7430bca6d7b80b04ef9146744a55102e935c71
dfclutch <filler.7@osu.edu>
2020-01-27 17:21:38 -0500

    moved question object out of app class

--- mockup/src/App.js
+++ mockup/src/App.js
@@ -5,59 +5,65 @@ import './components/QuestionCard/QuestionCard';
 import QuestionCard from "./components/QuestionCard/QuestionCard";
 import {INPUT_TYPES} from "./config";
 
+let questions = [
+  {
+    text: "I want...",
+    input_type: INPUT_TYPES.BUTTON_LIST,
+    options: [
+      "A recommendation based off previous books I've read",
+      "A recommendation based off my genre preferences"
+    ]
+  },
+  {
+    text: "What reading level are you looking for?",
+    input_type: INPUT_TYPES.BUTTON_LIST,
+    options: [
+      "Adult",
+      "Young Adult",
+      "Children"
+    ]
+  },
+  {
+    text: "What type of book are you looking for?",
+    input_type: INPUT_TYPES.BUTTON_LIST,
+    options: [
+      "Fiction",
+      "Non-fiction",
+      "I don't care."
+    ]
+  },
+  {
+    text: "What genres are you interested in?",
+    input_type: INPUT_TYPES.MULTISELECT,
+    options: [
+      "Romance",
+      "Science Fiction",
+      "Fantasy",
+      "Horror",
+      "Historical",
+      "Mystery",
+      "Adventure",
+      "Thriller",
+      "Self-help",
+      "Biographical",
+    ]
+  },
+  {
+    text: "I recommend: Pride and Prejudice"
+  }
+];
+
 class App extends Component{
-
-  questions = [
-    {
-      text: "I want...",
-      input_type: INPUT_TYPES.BUTTON_LIST,
-      options: [
-        "A recommendation based off previous books I've read",
-        "A recommendation based off my genre preferences"
-      ]
-    },
-    {
-      text: "What reading level are you looking for?",
-      input_type: INPUT_TYPES.BUTTON_LIST,
-      options: [
-        "Adult",
-        "Young Adult",
-        "Children"
-      ]
-    },
-    {
-      text: "What type of book are you looking for?",
-      input_type: INPUT_TYPES.BUTTON_LIST,
-      options: [
-        "Fiction",
-        "Non-fiction",
-        "I don't care."
-      ]
-    },
-    {
-      text: "What genres are you interested in?",
-      input_type: INPUT_TYPES.MULTISELECT,
-      options: [
-        "Romance",
-        "Science Fiction",
-        "Fantasy"
-      ]
-    },
-    {
-      text: "I recommend: Pride and Prejudice"
-    }
-  ];
-
   constructor(props) {
     super(props);
     this.state = {
-      current_question: this.questions.shift()
+      current_question: questions.shift()
     };
     this.nextQuestion = this.nextQuestion.bind(this);
   }
   nextQuestion() {
     this.setState({
-      current_question: this.questions.shift()
+      current_question: questions.shift()
     })
   }
 
